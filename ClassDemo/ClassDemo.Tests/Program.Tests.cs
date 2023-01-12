936441672d0994f1d90751c4a26cf61754b3b88f
Hunter T <strangeranger01@randomserver.xyz>
2023-01-12 15:33:38 -0800

    In class updates

--- ClassDemo/ClassDemo.Tests/Program.Tests.cs
+++ ClassDemo/ClassDemo.Tests/Program.Tests.cs
@@ -9,4 +9,18 @@ public class ProgramTests
         string actual = Program.GetName("Inigo", "Montoya");
         Assert.AreEqual<string>("Inigo Montoya", actual);
     }
+
+    [TestMethod]
+    public void PlayingWithIntegers()
+    {
+        int number = 42;
+        Assert.AreEqual("System.Int32", number.GetType().FullName);
+    }
+
+    [TestMethod]
+    public void PlayingWithStrings()
+    {
+        string number = "forty-two";
+        Assert.AreEqual("System.String", number.GetType().FullName);
+    }
 }
