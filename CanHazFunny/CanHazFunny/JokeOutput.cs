49b8322e49ac205041bf4d1130653e3498d08799
Hunter T <strangeranger01@randomserver.xyz>
2023-01-25 11:01:41 -0800

    Add PrintJoke() method

--- CanHazFunny/CanHazFunny/JokeOutput.cs
+++ CanHazFunny/CanHazFunny/JokeOutput.cs
@@ -1,8 +1,10 @@
-﻿namespace CanHazFunny;
+﻿using System;
+namespace CanHazFunny;
 
 public class JokeOutput : IJokeOutput
 {
-    
-    
-    
+    public void PrintJoke(string output)
+    {
+        Console.WriteLine(output);
+    }
 }
