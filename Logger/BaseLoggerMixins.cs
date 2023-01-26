84e62869e45920828297c68215433a74556d7136
Hunter T <strangeranger01@randomserver.xyz>
2023-01-25 23:18:01 -0800

    'int[]' are now 'object[]', allowing me to specify 'args' instead of 'args[0]'

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -3,36 +3,36 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger? baseLogger, string message, params int[] args)
+    public static void Error(this BaseLogger? baseLogger, string message, params object[] args)
     {
         if (baseLogger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Error;
-        baseLogger.Log(logLevel, string.Format(message, args[0]));
+        baseLogger.Log(logLevel, string.Format(message, args));
     }
 
-    public static void Warning(this BaseLogger? baseLogger, string message, params int[] args)
+    public static void Warning(this BaseLogger? baseLogger, string message, params object[] args)
     {
         if (baseLogger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Warning;
-        baseLogger.Log(logLevel, string.Format(message, args[0]));
+        baseLogger.Log(logLevel, string.Format(message, args));
     }
 
     public static void Information(this BaseLogger? baseLogger, string message,
-                                   params int[] args)
+                                   params object[] args)
     {
         if (baseLogger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Information;
-        baseLogger.Log(logLevel, string.Format(message, args[0]));
+        baseLogger.Log(logLevel, string.Format(message, args));
     }
 
-    public static void Debug(this BaseLogger? baseLogger, string message, params int[] args)
+    public static void Debug(this BaseLogger? baseLogger, string message, params object[] args)
     {
         if (baseLogger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Debug;
-        baseLogger.Log(logLevel, string.Format(message, args[0]));
+        baseLogger.Log(logLevel, string.Format(message, args));
     }
 }
