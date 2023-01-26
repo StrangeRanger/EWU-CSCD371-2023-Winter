d52671af6ee5f22847222a5a978ecd3ebb28313b
Hunter T <strangeranger01@randomserver.xyz>
2023-01-25 22:35:02 -0800

    Refactor variable

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -7,7 +7,7 @@ public class FileLogger : BaseLogger
     // Question: What is the purpose are the auto property? Should it always be public or is
     //           it ok to make it private.
     private string FilePath { get; set; }
-    
+
     public FileLogger(string filePath)
     {
         FilePath = filePath;
@@ -15,9 +15,9 @@ public class FileLogger : BaseLogger
 
     public override void Log(LogLevel logLevel, string message)
     {
-        DateTime date = DateTime.Now;
-        string outGoing =
-                $"{date} {ClassName} {Enum.GetName(typeof(LogLevel), logLevel)}: {message}";
+        DateTime date   = DateTime.Now;
+        ClassName       = nameof(FileLogger);
+        string outGoing = $"{date} {ClassName} {logLevel}: {message}";
 
         StreamWriter streamWriter = File.CreateText(FilePath);
         streamWriter.WriteLine(outGoing);
