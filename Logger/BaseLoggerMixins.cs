bbb8eb02d5c41b441a83c1dde4880328aee1ea17
Hunter T <strangeranger01@randomserver.xyz>
2023-02-01 11:27:30 -0800

    Rename parameter variable

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -3,37 +3,36 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger baseLogger, string message, params object[] args)
+    public static void Error(this BaseLogger logger, string message, params object[] args)
     {
-        if (baseLogger is null) { throw new ArgumentNullException(); }
+        if (logger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Error;
-        baseLogger.Log(logLevel, string.Format(message, args));
+        logger.Log(logLevel, string.Format(message, args));
     }
 
-    public static void Warning(this BaseLogger baseLogger, string message,
-                               params object[] args)
+    public static void Warning(this BaseLogger logger, string message, params object[] args)
     {
-        if (baseLogger is null) { throw new ArgumentNullException(); }
+        if (logger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Warning;
-        baseLogger.Log(logLevel, string.Format(message, args));
+        logger.Log(logLevel, string.Format(message, args));
     }
 
-    public static void Information(this BaseLogger baseLogger, string message,
+    public static void Information(this BaseLogger logger, string message,
                                    params object[] args)
     {
-        if (baseLogger is null) { throw new ArgumentNullException(); }
+        if (logger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Information;
-        baseLogger.Log(logLevel, string.Format(message, args));
+        logger.Log(logLevel, string.Format(message, args));
     }
 
-    public static void Debug(this BaseLogger baseLogger, string message, params object[] args)
+    public static void Debug(this BaseLogger logger, string message, params object[] args)
     {
-        if (baseLogger is null) { throw new ArgumentNullException(); }
+        if (logger is null) { throw new ArgumentNullException(); }
 
         LogLevel logLevel = LogLevel.Debug;
-        baseLogger.Log(logLevel, string.Format(message, args));
+        logger.Log(logLevel, string.Format(message, args));
     }
 }
