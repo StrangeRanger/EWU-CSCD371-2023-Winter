54313930d21e5a4ae091e82969073dc4a6ded7fc
Hunter T <strangeranger01@randomserver.xyz>
2023-02-01 12:33:02 -0800

    Revert previous change

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -1,15 +1,15 @@
+using System;
+using System.IO;
 namespace Logger;
 
 public class FileLogger : BaseLogger
 {
-    // Question: What is the purpose are the auto property? Should it always be public or is
-    //           it ok to make it private.
     private string _FilePath;
 
     public FileLogger(string filePath, string className)
     {
         ClassName = className;
-        _FilePath  = filePath;
+        _FilePath = filePath;
     }
 
     public override void Log(LogLevel logLevel, string message)
