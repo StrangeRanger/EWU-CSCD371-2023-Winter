41acc066f812b53a44d1cdff61f8c8d0e17931ea
Hunter Allen <hallen10@ewu.edu>
2023-02-02 11:11:30 -0800

    Changed full name to accept no middle name.

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -2,14 +2,22 @@
 
 public record struct FullName
 {
-    public FullName(string firstName, string middleName, string lastName)
+    public FullName(string firstName, string? middleName, string lastName)
     {
-        FirstName = firstName;
-        MiddleName = middleName;
-        LastName = lastName;
+        FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+        MiddleName = middleName ?? throw new ArgumentNullException(nameof(middleName));
+        LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
     }
-    
+    public FullName(string firstName, string lastName)
+    {
+        FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+        LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+    }
+    public string getFullName()
+    {
+        return $"{FirstName} {MiddleName} {LastName}";
+    }
     public string FirstName { get; init; }
-    public string MiddleName { get; init; }
+    public string? MiddleName { get; set; }
     public string LastName { get; init; }
 }
