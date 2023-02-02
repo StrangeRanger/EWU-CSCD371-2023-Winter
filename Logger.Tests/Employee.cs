3f7b337e8d3b825f497ff8903142a33eb84e3080
Hunter Allen <hallen10@ewu.edu>
2023-02-02 11:34:11 -0800

    Added empty tests.

---
+++ Logger.Tests/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class Employee
+{
+    [TestMethod]
+    public void Create_Employee()
+    {
+        var employee = new Employee();
+        Assert.AreEqual("John Doe Smith", employee.ToString());
+    }
+
+}
