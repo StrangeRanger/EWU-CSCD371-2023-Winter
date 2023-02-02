84bb6d46eba80c5ffb515b1edfd677317adaff0b
Hunter T <strangeranger01@randomserver.xyz>
2023-02-02 13:01:59 -0800

    Refactor constructors and properties

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,30 +1,24 @@
 ﻿namespace Logger;
 
-public record struct FullName
+public record struct FullName(string FirstName, string? MiddleName, string LastName)
 {
-    public FullName(string firstName, string? middleName, string lastName)
-    {
-        FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
-        MiddleName = middleName ?? throw new ArgumentNullException(nameof(middleName));
-        LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
-    }
-    public FullName(string firstName, string lastName)
+    /* Properties */
+    public string FirstName { get; } = FirstName ?? throw new ArgumentNullException(nameof(FirstName));
+    public string? MiddleName { get; } = MiddleName;
+    public string LastName { get; } = LastName ?? throw new ArgumentNullException(nameof(LastName));
+    
+    /* Constructor. */
+    public FullName(string firstName, string lastName) : this(firstName, null, lastName)
     {
         FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
         LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
     }
     public override string ToString()
     {
-        if (MiddleName is null)
-        {
+        if (MiddleName is null) {
             return $"{FirstName} {LastName}";
-        }
-        else
-        {
+        } else {
             return $"{FirstName} {MiddleName} {LastName}";
         }
     }
-    public string FirstName { get; init; }
-    public string? MiddleName { get; set; }
-    public string LastName { get; init; }
 }
