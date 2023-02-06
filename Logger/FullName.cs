565118d66652d3266b5d1fa2e021eb62bc6c7efb
Hunter Allen <hallen10@ewu.edu>
2023-02-06 11:05:49 -0800

    Fixed Full Name

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -4,4 +4,6 @@ public record struct FullName(string FirstName, string LastName, string? MiddleN
     public string FirstName { get; } = FirstName ?? throw new ArgumentNullException(nameof(FirstName));
     public string LastName { get; } = LastName ?? throw new ArgumentNullException(nameof(LastName));
     public string? MiddleName { get; } = MiddleName;
+    
+    public override string ToString() => MiddleName == null ? $"{FirstName} {LastName}" : $"{FirstName} {MiddleName} {LastName}";
 }
