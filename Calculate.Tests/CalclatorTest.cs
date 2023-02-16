5e5dd0622f90cc2834c616d4be0d96895807be74
Hunter T <strangeranger01@randomserver.xyz>
2023-02-16 13:27:43 -0800

    TMP

--- Calculate.Tests/CalclatorTest.cs
+++ Calculate.Tests/CalclatorTest.cs
@@ -4,6 +4,24 @@ namespace Calculate.Tests;
 public class CalculatorTest
 {
     [TestMethod]
-    public void TestMethod1()
-    { }
+    public void Calculate_Int()
+    {
+        Calculator<int> calculator = new();
+        int total = calculator.Add(1, 2);
+
+        Assert.AreEqual(3, total);
+    }
+    
+    [TestMethod]
+    public void Calculate_Double()
+    {
+
+    }
+    
+    [TestMethod]
+    public void Calculate_Float()
+    { 
+
+        
+    }
 }
--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -1,4 +1,37 @@
 namespace Calculate;
 
-public class Calculator
-{ }
+public class Calculator<TValue>
+{
+
+    public IReadOnlyDictionary<char, TValue> MathematicalOperations { get; } = new Dictionary<char, TValue>();
+    
+    public TValue Add(TValue a, TValue b)
+    {
+        return a + b;
+    }
+    
+    public TValue Subtract(TValue a, TValue b)
+    {
+        return a - b;
+    }
+
+    public TValue Multiply(TValue a, TValue b)
+    {
+        return a * b;
+    }
+
+    public TValue Divide(TValue a, TValue b)
+    {
+        int.TryParse(a, b);
+        return a / b;
+    }
+
+    
+    
+    public void TryCalculate()
+    {
+        
+    }
+    
+    
+}
