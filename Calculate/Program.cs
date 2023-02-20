ab77aa6498f495112147d327127547d8f99b170a
Hunter T <strangeranger01@randomserver.xyz>
2023-02-20 10:57:59 -0800

    Remove redundant type specifier

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -2,6 +2,6 @@ namespace Calculate;
 
 public class Program
 {
-    public Action<string> WriteLine { get; init; } = (string s) => Console.WriteLine(s);
+    public Action<string> WriteLine { get; init; } = s => Console.WriteLine(s);
     public Func<string?> ReadLine { get; init; } = () => Console.ReadLine();
 }
