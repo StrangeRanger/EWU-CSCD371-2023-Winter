af41cce1935297a43586fee37a6f7e3eeb385434
Hunter T <strangeranger01@randomserver.xyz>
2023-02-23 11:11:48 -0800

    Add empty constructor as specified by assignment instructions

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -4,4 +4,10 @@ public class Program
 {
     public Action<string> WriteLine { get; init; } = Console.WriteLine;
     public Func<string?> ReadLine { get; init; } = Console.ReadLine;
+
+    // ASSIGNMENT QUESTION: Is this empty constructor needed? The default constructor is created by
+    //                      default during compilation, right? Wouldn't that make this constructor
+    //                      redundant?
+    public Program()
+    { }
 }
