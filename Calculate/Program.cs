5b2d0ad73d8802a238f9edbd25547b0cfda28657
Hunter T <strangeranger01@randomserver.xyz>
2023-02-23 11:13:27 -0800

    Revert 'Convert into method group'

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -2,8 +2,8 @@ namespace Calculate;
 
 public class Program
 {
-    public Action<string> WriteLine { get; init; } = Console.WriteLine;
-    public Func<string?> ReadLine { get; init; } = Console.ReadLine;
+    public Action<string> WriteLine { get; init; } = s => Console.WriteLine(s);
+    public Func<string?> ReadLine { get; init; } = () => Console.ReadLine();
 
     // ASSIGNMENT QUESTION: Is this empty constructor needed? The default constructor is created by
     //                      default during compilation, right? Wouldn't that make this constructor
