31b1015e5a45c0f26250008cb89a1ba008b145e5
Hunter Allen <hallen10@ewu.edu>
2023-03-03 10:46:26 -0800

    Added node with tests.

--- Assignment/Assignment.Tests/NodeTest.cs
+++ Assignment/Assignment.Tests/NodeTest.cs
@@ -1,6 +1,86 @@
-﻿namespace Assignment.Tests;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Assignment.Tests;
+
+[TestClass]
 public class NodeTest
 {
+    [ExpectedException(typeof(ArgumentNullException))]
+    [TestMethod]
+    public void Node_Constructor_NullValue_ThrowsArgumentNullException()
+    {
+        Node<string> node = new Node<string>(null !);
+    }
     
+    [TestMethod]
+    public void Node_Constructor_ValidValue_SetsValue()
+    {
+        Node<string> node = new Node<string>("Test");
+        Assert.AreEqual("Test", node.ToString());
+    }
+    
+    [TestMethod]
+    public void Node_Constructor_ValidValue_SetsNextToSelf()
+    {
+        Node<string> node = new Node<string>("Test");
+        Assert.AreEqual(node, node.Next);
+    }
+    
+    [TestMethod]
+    public void Node_Append_ValidValue_SetsNextToNewNode()
+    {
+        Node<string> node = new Node<string>("Test");
+        node.Append("Test2");
+        Assert.AreEqual("Test2", node.Next.ToString());
+    }
+    
+    [TestMethod]
+    public void Nodes_AreCircular_IsTrue()
+    {
+        Node<string> node = new Node<string>("Test");
+        node.Append("Test2");
+        node.Append("Test3");
+        Assert.AreEqual("Test", node.ToString());
+        Assert.AreEqual("Test2", node.Next.ToString());
+        Assert.AreEqual("Test3", node.Next.Next.ToString());
+        Assert.AreEqual("Test", node.Next.Next.Next.ToString());
+        Assert.AreEqual("Test2", node.Next.Next.Next.Next.ToString());
+    }
+    
+    [ExpectedException(typeof(ArgumentException))]
+    [TestMethod]
+    public void Node_Append_DuplicateValue_ThrowsArgumentException()
+    {
+        Node<string> node = new Node<string>("Test");
+        node.Append("Test2");
+        node.Append("Test2");
+    }
+    
+    [TestMethod]
+    public void Node_AllChildItems_ReturnsCorrectNumberOfItems()
+    {
+        Node<string> node = new Node<string>("Test");
+        node.Append("Test2");
+        node.Append("Test3");
+        node.Append("Test4");
+        node.Append("Test5");
+        node.Append("Test6");
+        Assert.AreEqual(6, node.AllChildItems().Count());
+    }
+
+    [TestMethod]
+    public void Node_ChildItems_ReturnsCorrectNumberOfItems()
+    {
+        Node<string> node = new Node<string>("Test");
+        node.Append("Test2");
+        node.Append("Test3");
+        node.Append("Test4");
+        node.Append("Test5");
+        node.Append("Test6");
+        IEnumerable<string> childItems = node.ChildItems(3);
+        Assert.AreEqual(3, childItems.Count());
+    }
 }
--- Assignment/Assignment/Node.cs
+++ Assignment/Assignment/Node.cs
@@ -1,6 +1,83 @@
-﻿namespace Assignment;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Node
+namespace Assignment;
+
+public class Node<T>
 {
+    public Node(T value)
+    {
+        _Value = value == null ? throw new ArgumentNullException(nameof(value)) : value;
+        Next = this;
+    }
+
+    private T _Value{ get; set;}
+    public Node<T> Next { get; private set; } 
     
+    public void Append(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+        newNode.Next = this;
+        
+        Node<T> currentNode = this;
+        while (currentNode.Next != this)
+        {
+            if(currentNode.Next._Value!.Equals(value))
+            {
+                throw new ArgumentException("Value already exists in list");
+            }
+            currentNode = currentNode.Next;
+        }
+        currentNode.Next = newNode;
+    }
+
+    public IEnumerable<T> AllChildItems()
+    {
+        Node<T> currentNode = this;
+        yield return currentNode._Value!;
+        while (currentNode.Next != this)
+        {
+            yield return currentNode.Next._Value!;
+            currentNode = currentNode.Next;
+        }
+    }
+    
+    public IEnumerable<T> ChildItems(int maximum)
+    {
+        Node<T> currentNode = this;
+        yield return currentNode._Value!;
+        maximum--;
+        while (currentNode.Next != this && maximum > 0)
+        {
+            yield return currentNode.Next._Value!;
+            currentNode = currentNode.Next;
+            maximum--;
+        }
+    }
+    
+    public override string ToString()
+    {
+        return _Value!.ToString()!;
+    }
+    
+    public void RemoveAllChildItems()
+    {
+        Next = this;
+    }
+    
+    public bool Exist(T value)
+    {
+        Node<T> currentNode = this;
+        while(currentNode.Next != this)
+        {
+            if(currentNode.Next._Value!.Equals(value))
+            {
+                return true;
+            }
+            currentNode = currentNode.Next;
+        }
+        return false;
+    }
 }
