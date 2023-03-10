0048ff4be0c1f0d707b61a5c66615e10ad856fff
Hunter T <strangeranger01@randomserver.xyz>
2023-03-09 20:38:10 -0800

    Several changes
    - Replace long LINQ with Skip() LINQ method
    - Fix formatting

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -8,27 +8,24 @@ namespace Assignment
 {
     public class SampleData : ISampleData
     {
-        public IEnumerable<string> CsvRows { get; } = 
-            from string line in File.ReadAllLines("People.csv")
-            where !line.Contains("Id,FirstName,LastName,Email,StreetAddress,City,State,Zip")
-            select line;
-        
+        public IEnumerable<string> CsvRows { get; } = File.ReadAllLines("People.csv").Skip(1);
+
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
         {
             return (from string item in CsvRows 
-                    let state = item.Split(',')[6] 
-                    orderby state 
+                    let state = item.Split(',')[6]
+                    orderby state
                     select state).Distinct();
         }
-        
+
         public string GetAggregateSortedListOfStatesUsingCsvRows()
         {
             return string.Join(", ", GetUniqueSortedListOfStatesGivenCsvRows().ToArray());
         }
-        
-        public IEnumerable<IPerson> People => from string item in CsvRows
-                                              let split = item.Split(',')
-                                              orderby split[6], item[5], item[7]
+
+        public IEnumerable<IPerson> People => from string item in CsvRows 
+                                              let split = item.Split(',') 
+                                              orderby split[6], item[5], item[7] 
                                               select new Person(split[1], 
                                                                 split[2], 
                                                                 new Address(split[4], 
@@ -37,16 +34,18 @@ namespace Assignment
                                                                             split[7]), 
                                                                 split[3]);
 
-        
-        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
+
+        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
+            Predicate<string> filter)
         {
             IEnumerable<(string FirstName, string LastName)> names =
                 from Person person in People
                 where filter(person.EmailAddress)
-                select (person.FirstName, person.LastName);
+                select(person.FirstName, person.LastName);
+            
             return names;
         }
-        
+
         public string GetAggregateListOfStatesGivenPeopleCollection(IEnumerable<IPerson> people)
         {
             return people.Select(p => p.Address.State).Distinct().Aggregate((a, b) => a + ", " + b);
